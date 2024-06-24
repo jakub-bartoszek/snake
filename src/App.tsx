a3cb254b7bc69bba9cf2c5f810f15c3dc97a4965
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-06-24 15:53:25 +0200

    change: cleared App and added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Game from "./Game/Game";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+  <div className="flex items-center justify-center h-full bg-gradient-to-br from-[#8e1e5b] to-[#0292c2]">
+   <Game />
+  </div>
+ );
 }
 
 export default App;
